b7b356e48a0dc778a91160fefd0d5e8c880edc4b
gmandonnan <gabedonnan@hotmail.co.uk>
2024-08-16 16:17:18 +0100

    Staticfiles moment N+N2

--- dissdjango/settings.py
+++ dissdjango/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "daphne",
-    "django.contrib.staticfiles",
     "channels",
 ]
 
@@ -120,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = str(BASE_DIR / "static")
+STATIC_URL = ""
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
