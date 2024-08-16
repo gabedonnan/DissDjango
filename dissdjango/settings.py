5adb1f6439d0c84e855e56482b8033b685d06171
gmandonnan <gabedonnan@hotmail.co.uk>
2024-08-16 15:52:40 +0100

    Staticfiles moment N+N

--- dissdjango/settings.py
+++ dissdjango/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = str(BASE_DIR / "sim/templates/sim")
+STATIC_URL = str(BASE_DIR / "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
