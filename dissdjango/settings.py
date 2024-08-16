3a7cb63331d515c2aab9917d8c44bf1bdc80653c
gmandonnan <gabedonnan@hotmail.co.uk>
2024-08-16 15:37:22 +0100

    Staticfiles moment N

--- dissdjango/settings.py
+++ dissdjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f-q7_b%ojbxs@*yt$j#1vp!pwog!zm(c%*44fm9r0a24k*x5r7"
+SECRET_KEY = "JaPZ4@?2w9%&BScjr_[phN"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
