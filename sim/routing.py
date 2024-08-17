e487f96a123e5c6f0ad51da3318c1439ca9e8e89
gmandonnan <gabedonnan@hotmail.co.uk>
2024-08-17 16:54:35 +0100

    Websocket url change

--- sim/routing.py
+++ sim/routing.py
@@ -3,9 +3,9 @@ from django.urls import re_path
 from .websocket_consumers import SimConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/dutch/(?P<room_name>\w+)", SimConsumer.as_asgi()),
-    re_path(r"ws/english/(?P<room_name>\w+)", SimConsumer.as_asgi()),
-    re_path(r"ws/fpsb/(?P<room_name>\w+)", SimConsumer.as_asgi()),
-    re_path(r"ws/spsb/(?P<room_name>\w+)", SimConsumer.as_asgi()),
-    re_path(r"ws/cda/(?P<room_name>\w+)", SimConsumer.as_asgi()),
+    re_path(r"(?P<domain_prefix>\w+)/ws/dutch/(?P<room_name>\w+)", SimConsumer.as_asgi()),
+    re_path(r"(?P<domain_prefix>\w+)/ws/english/(?P<room_name>\w+)", SimConsumer.as_asgi()),
+    re_path(r"(?P<domain_prefix>\w+)/ws/fpsb/(?P<room_name>\w+)", SimConsumer.as_asgi()),
+    re_path(r"(?P<domain_prefix>\w+)/ws/spsb/(?P<room_name>\w+)", SimConsumer.as_asgi()),
+    re_path(r"(?P<domain_prefix>\w+)/ws/cda/(?P<room_name>\w+)", SimConsumer.as_asgi()),
 ]
