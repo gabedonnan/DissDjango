0753e2414eeacd40b61f1ed1ce29cb6298562137
gmandonnan <gabedonnan@hotmail.co.uk>
2024-08-23 16:15:19 +0100

    fixed FPSB, SPSB, CDA routing pt 2

--- sim/routing.py
+++ sim/routing.py
@@ -5,7 +5,7 @@ from .websocket_consumers import SimConsumer
 websocket_urlpatterns = [
     re_path(r"ws/dutch/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
     re_path(r"ws/english/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
-    re_path(r"ws/fpsb/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
-    re_path(r"ws/spsb/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
-    re_path(r"ws/cda/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
+    re_path(r"ws/FPSB/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
+    re_path(r"ws/SPSB/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
+    re_path(r"ws/CDA/(?P<room_name>\w+)/$", SimConsumer.as_asgi()),
 ]
