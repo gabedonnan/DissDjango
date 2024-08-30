133a030d78efd2104c73dd47f4660192b92ffd49
gmandonnan <gabedonnan@hotmail.co.uk>
2024-08-30 16:44:42 +0100

    Removed object size debugging

--- sim/websocket_consumers.py
+++ sim/websocket_consumers.py
@@ -18,38 +18,6 @@ auction_instances: dict[str, Auction | None | ContinuousDoubleAuction] = {}
 connection_counters: dict[str, int] = defaultdict(int)
 
 
-from numbers import Number
-from collections import deque
-from collections.abc import Set, Mapping
-
-
-ZERO_DEPTH_BASES = (str, bytes, Number, range, bytearray)
-
-
-def getsize(obj_0):
-    """Recursively iterate to sum size of object & members."""
-    _seen_ids = set()
-    def inner(obj):
-        obj_id = id(obj)
-        if obj_id in _seen_ids:
-            return 0
-        _seen_ids.add(obj_id)
-        size = sys.getsizeof(obj)
-        if isinstance(obj, ZERO_DEPTH_BASES):
-            pass # bypass remaining control flow and return
-        elif isinstance(obj, (tuple, list, Set, deque)):
-            size += sum(inner(i) for i in obj)
-        elif isinstance(obj, Mapping) or hasattr(obj, 'items'):
-            size += sum(inner(k) + inner(v) for k, v in getattr(obj, 'items')())
-        # Check for custom object instances - may subclass above too
-        if hasattr(obj, '__dict__'):
-            size += inner(vars(obj))
-        if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
-            size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
-        return size
-    return inner(obj_0)
-
-
 class SimConsumer(AsyncWebsocketConsumer):
     room_id: str
     room_group_id: str
@@ -99,7 +67,6 @@ class SimConsumer(AsyncWebsocketConsumer):
     ):
         room_name = self.scope["url_route"]["kwargs"]["room_name"]
         sim = auction_instances[room_name]
-        print("SIM SIZE", getsize(sim))
 
         # Format {username: ..., message: {...}}
         parsed_data = json.loads(text_data)
